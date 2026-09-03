bdbf534e7456d58b728e511fb083f48225b58a7f
Mindy <wade@lev-15584.apn.wlan.private.upenn.edu>
2026-09-03 15:29:24 -0400

    hsv_mask modified part2

--- part2_video.py
+++ part2_video.py
@@ -1,7 +1,12 @@
 import cv2
-from src.detector import detect_shapes_in_frame
+import numpy as np
+from src.detector import detect_shapes_with_grayscale_mask
 
 cap = cv2.VideoCapture("assets/PennAir 2024 App Dynamic.mp4")
+
+lower_hsv = np.array([0, 0, 0])
+# Upper HSV boundary: [Hue=105, Saturation=243, Value=155]
+upper_hsv = np.array([105, 243, 155])
 
 while cap.isOpened():
     ret, frame = cap.read()
@@ -9,7 +14,7 @@ while cap.isOpened():
         break  # End of video stream
 
     # Process frame frame-by-frame
-    processed_frame, centers = detect_shapes_in_frame(frame, background_agnostic=False)
+    processed_frame, centers, mask = detect_shapes_with_grayscale_mask(frame,lower_hsv, upper_hsv)
 
     cv2.imshow("Part 2 - Video Stream Shape Detection", processed_frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
